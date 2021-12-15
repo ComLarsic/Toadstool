2f60270ee9a2dfa7168c97b9a6fd67df91003c3e
ComLarsic <larsjanjensen@gmail.com>
2021-12-15 21:15:54 +0100

    Add a proper setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='toadstool',
     version='0.1.0',
     description='A simple bevy-inspired ecs for python.',
+    url="https://github.com/ComLarsic/Toadstool",
     author='ComLarsic',
+    author_email="larsjanjensen@protonmail.com",
     license='MIT',
+    packages=find_packages(include=["toadstool", "toadstool.*"])
 )
